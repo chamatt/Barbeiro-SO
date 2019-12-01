9fee3f04ad74a25cf961a8dc270ccec80aa74369
Pedro Anselmo <pedro.anselmo@leanonsystems.com>
2019-12-01 14:06:49 -0300

    Adding main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		 int qtdBarbeiros = Integer.parseInt(args[0]);
+		 int qtdCadeiras = Integer.parseInt(args[1]);
+		 int qtdClientes = Integer.parseInt(args[2]);
+		 
+		 
+	}
+
+}
